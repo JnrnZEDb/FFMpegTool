fc1fa9d48033df658fe0b51c4a2a1c562f5bf93c
Mina Shoaib Rahman <mirahman@medavante.com>
2017-08-16 18:08:24 +0600

    Initial commit

---
+++ FFMpegTest/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace FFMpegTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Video Duration";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.Location = new System.Drawing.Point(66, 203);
+            this.labelLength.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(40, 13);
+            this.labelLength.TabIndex = 1;
+            this.labelLength.Text = "Length";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(69, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "First";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Second";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 250);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(777, 289);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Log";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(121, 203);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(30, 13);
+            this.labelTime.TabIndex = 10;
+            this.labelTime.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 551);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelLength);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ FFMpegTest/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFMpegTest
+{
+    public partial class Form1 : Form
+    {
+        private string VideoPath = "C:\\Windows\\Temp\\";
+        private const string VideoFileExtension = ".mp4";
+
+        private string _tempFolder = "C:\\Windows\\Temp";
+        private const string PrepareToCombineTemplate = "-i \"{0}\" -c copy -bsf h264_mp4toannexb -y \"{1}\"";
+        private const string CombineTemplate = "-i \"concat:\"{0}\"\" -c copy -bsf:a aac_adtstoasc -y \"{1}\"";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintLog(string log)
+        {
+            if(richTextBox1.Text == string.Empty)
+                richTextBox1.Text = log;
+            else
+                richTextBox1.Text += "\n" + log;
+        }
+
+        private int ParseDurationLine(string line)
+        {
+            var items = line.Split(" "[0], "="[0]).Where(s => string.IsNullOrEmpty(s) == false).
+                Select(s => s.Trim().Replace("=", string.Empty).Replace(",", string.Empty)).ToList();
+
+            var key = items.FirstOrDefault(i => i.ToUpper() == "Duration:".ToUpper());
+
+            if (key == null)
+                return 0;
+            var idx = items.IndexOf(key) + 1;
+            if (idx >= items.Count)
+                return 0;
+
+            return (int)TimeSpan.Parse(items[idx]).TotalSeconds;
+        }
+
+        private int ExecuteFfmpegCommand(string args, bool parseDuration = false)
+        {
+            using (var ffmpeg = new Process())
+            {
+                ffmpeg.StartInfo = new ProcessStartInfo()
+                {
+                    FileName = new FFMpeg.FFMpegWrapper().FFMpegExe,
+                    RedirectStandardOutput = false,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    Arguments = args
+                };
+
+                ffmpeg.Start();
+                var standardError = ffmpeg.StandardError.ReadToEnd();
+                ffmpeg.WaitForExit();
+                EnsureFFMpegDone(ffmpeg.ExitCode, standardError);
+
+                if (parseDuration)
+                {
+                    var encodingLines =
+                    standardError.Split(System.Environment.NewLine[0])
+                        .Where(
+                            line =>
+                                string.IsNullOrWhiteSpace(line) == false &&
+                                string.IsNullOrEmpty(line.Trim()) == false)
+                        .Select(s => s.Trim())
+                        .ToList();
+                    foreach (var line in encodingLines)
+                    {
+                        if (line.StartsWith("Duration"))
+                        {
+                            return ParseDurationLine(line);
+                        }
+                    }
+                }
+
+                return 0;
+            }
+        }
+
+        private void EnsureFFMpegDone(int exitCode, string standardError)
+        {
+            if (exitCode != 0)
+                PrintLog(string.Format("ffmpeg.exe has failed with the error: {0}", standardError ?? ""));
+        }
+
+        private void Combine(string[] videos, string resultVideoPath)
+        {
+            PrintLog("Combine videos into the single file");
+
+            var tempVideos = new List<string>();
+            var args = "";
+
+            foreach (var video in videos)
+            {
+                // Creating .ts (part file) as new Guid to avoid files containing space in it
+                var tempFileName = String.Format(CultureInfo.InvariantCulture, "{0}.ts", Guid.NewGuid());
+
+                var tempVideoPath = Path.Combine(_tempFolder, tempFileName);
+                tempVideos.Add(tempVideoPath);
+
+                args = String.Format(CultureInfo.InvariantCulture, PrepareToCombineTemplate, video, tempVideoPath);
+
+                PrintLog(String.Format(CultureInfo.InvariantCulture, "Execute: ffmpeg.exe {0}", args));
+
+                ExecuteFfmpegCommand(args);
+            }
+
+
+            args = String.Format(CultureInfo.InvariantCulture, CombineTemplate, String.Join("\"|\"", tempVideos), resultVideoPath);
+
+            ExecuteFfmpegCommand(args);
+
+            // Deleting local part files (.ts extension)
+            for (int i = 0; i < tempVideos.Count; i++)
+            {
+                if (File.Exists(tempVideos[i]))
+                {
+                    PrintLog(string.Format("Removing: {0}", tempVideos[i]));
+                    File.Delete(tempVideos[i]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name1 = textBox1.Text;
+            string name2 = textBox2.Text;
+            string result = textBox3.Text;
+
+            List<string> videos = new List<string>();
+            videos.Add(VideoPath + name1 + VideoFileExtension);
+            videos.Add(VideoPath + name2 + VideoFileExtension);
+
+            richTextBox1.Text = "";
+
+            var watch = Stopwatch.StartNew();
+            Combine(videos.ToArray(), VideoPath + result + VideoFileExtension);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            labelTime.Text = elapsedMs.ToString();
+
+            labelLength.Text = ExecuteFfmpegCommand("-y -i \"" + VideoPath + result + VideoFileExtension + "\"", true).ToString();
+        }
+    }
+}
